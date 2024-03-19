6f16156b2e00df571a057e0fc0f4502aae4e2fad
khaituonq <khaituong2909@gmail.com>
2024-03-19 13:45:54 +0700

    Update index.js file

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "realtime-chat-b4wgq2hmm-khaituonq.vercel.app",
     credentials: true,
   },
 });
